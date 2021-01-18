5b1c4ba0abfb2949b865557b8850c4f83bd3871a
Anton Semenov <superjuk@gmail.com>
2021-01-19 00:21:48 +0300

    Add currency converter

--- GetCourse.py
+++ GetCourse.py
@@ -7,9 +7,11 @@ import os
 import configparser as cp
 
 from telebot import types
+from multiprocessing import Value
 from timeloop import Timeloop
 from datetime import timedelta
 
+# Инициализация
 # Чтение конфига
 config = cp.ConfigParser(interpolation=None)
 config.read('settings.conf')
@@ -17,6 +19,9 @@ getCourseUrl = config['main']['url']
 token = config['telegram']['token']
 
 bot = telebot.TeleBot(token)
+
+usdSell = Value('d', 0.01)
+eurSell = Value('d', 0.01)
 
 usdCsvDir = os.getcwd()+'\\USD\\'
 if not os.path.exists(usdCsvDir):
@@ -46,6 +51,12 @@ def saveCourseToCsv(currency, date, data):
     csv.write(raw)
     csv.close()
 
+def convert(raw, course, currencyName):
+    array = raw.split()
+    if len(array) != 2:
+        return 'Неверный формат данных.\n Попробуйте отправить, например, '"'100 rub'"'\n'
+    return format(float(array[0]) * course, '.2f') + ' ' + currencyName + '\n'
+
 # Получение курса валюты
 def getCourse():
     # Запрос курса валюты
@@ -69,18 +80,20 @@ def getCourse():
         date = dt.datetime.fromisoformat(moneyRate[0]['StartDate'])
     
         if fromCode == usd:
+            with usdSell.get_lock():
+                usdSell.value = sell
             usdRate.append(dt.datetime.now().strftime('%X'))
             usdRate.append(date.strftime('%X'))
-            #usdRate.append(fromCode)
             usdRate.append(str(sell))
             usdRate.append(str(buy))
             saveCourseToCsv(fromCode, date.strftime('%Y-%m-%d'), usdRate)
             breakCount -= 1
     
         if fromCode == eur:
+            with eurSell.get_lock():
+                eurSell.value = sell
             eurRate.append(dt.datetime.now().strftime('%X'))
             eurRate.append(date.strftime('%X'))
-            #eurRate.append(fromCode)
             eurRate.append(str(sell))
             eurRate.append(str(buy))
             saveCourseToCsv(fromCode, date.strftime('%Y-%m-%d'), eurRate)
@@ -104,9 +117,21 @@ def getCourse():
 # Обработка входящего сообщения
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    factor = 0.0
     if message.text == 'Текущий курс':
         response = getCourse()
         bot.send_message(message.from_user.id, response)
+    elif 'rub' in message.text.lower() or 'rur' in message.text.lower():
+        factor = 1/usdSell.value
+        bot.send_message(message.from_user.id, convert(message.text, factor, 'usd'))
+        factor = 1/eurSell.value
+        bot.send_message(message.from_user.id, convert(message.text, factor, 'eur'))
+    elif 'usd' in message.text.lower():
+        factor = usdSell.value
+        bot.send_message(message.from_user.id, convert(message.text, factor, 'rur'))
+    elif 'eur' in message.text.lower():
+        factor = eurSell.value
+        bot.send_message(message.from_user.id, convert(message.text, factor, 'rur'))
     else:
         bot.send_message(message.from_user.id, 'Ошибка!')
 
@@ -114,6 +139,9 @@ def get_text_messages(message):
 def start_bot_polling():
     print('polling start\n')
     bot.polling(none_stop=True, interval=0)
+
+# __run__
+getCourse()
 
 t = threading.Thread(target=start_bot_polling, daemon=True)
 t.start()
@@ -129,4 +157,8 @@ tl.start(block=True)
 # Завершение работы скрипта
 bot.stop_polling()
 t.join()
+usdSell.close()
+usdSell.unlink()
+eurSell.close()
+eurSell.unlink()
 print('polling end\n')
